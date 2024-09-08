4ff5cb45d9747371e33b4e744b7e0d82db312053
Matthew Wellendorf <mwellend@uci.edu>
2024-09-08 15:21:56 -0400

    Rearranged layout, added smooth scrolling

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,76 @@
 import Image from "next/image";
 import Header from "@/components/header";
-import Home from "@/components/home";
-import About from "@/components/about";
+import Github from "../public/github.png";
+import LinkedIn from "../public/linkedin.png";
+import Programming from "../public/programming.png";
 
 export default function Portfolio() {
   return (
-    <div className="flex flex-col gap-16">
+    <div>
       <Header></Header>
-      <Home></Home>
-      <About></About>
+      <div className="pt-44 flex flex-col gap-8">
+        <div className="flex flex-col w-screen justify-center items-center gap-8 text-black pt-[150px]" id="home">
+          <h1 className="font-bold text-4xl">Home</h1>
+          <Image src={Programming} alt="Programming"></Image>
+          <p 
+              className="max-w-[500px] min-w-52"
+          >
+          I am a Front-End / Full-Stack Developer currently interning for Geaux Network and looking for a full time 
+          developer position.
+          </p>
+          <div className="flex gap-8">
+              <a href="https://github.com/wellendorfmatthew?tab=repositories" className="hover:bg-thistle hover:rounded-full duration-300">
+                  <Image src={Github} alt="Github"></Image>
+              </a>
+              <a href="https://www.linkedin.com/in/matthew-wellendorf-7a9ba828a/" className="hover:bg-thistle hover:rounded-full duration-300">
+                  <Image src={LinkedIn} alt="LinkedIn"></Image>
+              </a>
+          </div>
+          <a 
+              href="/softwareresume.pdf" 
+              target="_blank" 
+              className="bg-thistle text-lg px-8 py-4 rounded-full font-bold hover:drop-shadow-xl duration-150"
+          >
+          Resume
+          </a>
+        </div>
+        <div className="flex flex-col w-screen justify-center items-center gap-8 text-black pt-[150px]" id="about">
+            <h1 className="font-bold text-4xl">About</h1>
+            <p 
+                className="max-w-[500px] min-w-52"
+            >
+            Entry-level Software Engineer experienced in using HTML and CSS to build responsive web applications and eager to use
+            React, Javascript, Node.js and other technologies to solve complex problems and make meaningful contributions.
+            </p>
+        </div>
+        <div className="flex flex-col w-screen justify-center items-center gap-8 text-black pt-[150px]" id="skills">
+            <h1 className="font-bold text-4xl">Skills</h1>
+            <p 
+                className="max-w-[500px] min-w-52"
+            >
+            Entry-level Software Engineer experienced in using HTML and CSS to build responsive web applications and eager to use
+            React, Javascript, Node.js and other technologies to solve complex problems and make meaningful contributions.
+            </p>
+        </div>
+        <div className="flex flex-col w-screen justify-center items-center gap-8 text-black pt-[150px]" id="projects">
+            <h1 className="font-bold text-4xl">Projects</h1>
+            <p 
+                className="max-w-[500px] min-w-52"
+            >
+            Entry-level Software Engineer experienced in using HTML and CSS to build responsive web applications and eager to use
+            React, Javascript, Node.js and other technologies to solve complex problems and make meaningful contributions.
+            </p>
+        </div>
+        <div className="flex flex-col w-screen justify-center items-center gap-8 text-black pt-[150px]" id="contact">
+            <h1 className="font-bold text-4xl">Contact</h1>
+            <p 
+                className="max-w-[500px] min-w-52"
+            >
+            Entry-level Software Engineer experienced in using HTML and CSS to build responsive web applications and eager to use
+            React, Javascript, Node.js and other technologies to solve complex problems and make meaningful contributions.
+            </p>
+        </div>
+      </div>
     </div>
   );
 }
--- components/header.tsx
+++ components/header.tsx
@@ -7,47 +7,49 @@ export default function Header() {
 
         links.forEach(link => {
             link.classList.remove('underline');
+            link.classList.remove('font-bold');
         })
 
         const clickedLink = document.querySelector(`.link-${link}`);
         clickedLink?.classList.add('underline')
+        clickedLink?.classList.add('font-bold')
     }
 
     return (
-        <div className="bg-thistle flex w-screen h-36 items-center justify-evenly text-black text-lg">
+        <div className="bg-thistle flex w-screen h-36 items-center justify-evenly text-black text-lg fixed top-0">
             <h1 className="font-bold">Matthew Wellendorf</h1>
             <div className="flex items-center gap-12">
                 <a 
                     href="#home" 
-                    className="underline-offset-8 decoration-4 link link-0" 
+                    className="underline-offset-8 decoration-4 link link-0 hover:font-bold duration-150" 
                     onClick={() => handleLink(0)}
                 >
                     Home
                 </a>
                 <a 
                     href="#about" 
-                    className="underline-offset-8 decoration-4 link link-1" 
+                    className="underline-offset-8 decoration-4 link link-1 hover:font-bold duration-150" 
                     onClick={() => handleLink(1)}
                 >
                     About
                 </a>
                 <a 
                     href="#skills" 
-                    className="underline-offset-8 decoration-4 link link-2" 
+                    className="underline-offset-8 decoration-4 link link-2 hover:font-bold duration-150" 
                     onClick={() => handleLink(2)}
                 >
                     Skills
                 </a>
                 <a 
                     href="#projects" 
-                    className="underline-offset-8 decoration-4 link link-3" 
+                    className="underline-offset-8 decoration-4 link link-3 hover:font-bold duration-150" 
                     onClick={() => handleLink(3)}
                 >
                     Projects
                 </a>
                 <a 
                     href="#contact" 
-                    className="underline-offset-8 decoration-4 link link-4" 
+                    className="underline-offset-8 decoration-4 link link-4 hover:font-bold duration-150" 
                     onClick={() => handleLink(4)}
                 >
                     Contact
